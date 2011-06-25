5f12755ed7df2dc985b20f1fcc19d9f6480efa3e
bill.mania <bill.mania@191b9dcc-e101-4cc4-a847-655cb88f2929>
2011-06-25 14:29:12 +0000

    more intuitive name

---
+++ sensors/vision/BlobTracking/BlobTrackerTest.py
@@ -0,0 +1,22 @@
+'''
+Created on May 26, 2011
+
+@author: kinzle_eric
+'''
+
+from BlobTracker import *
+
+if __name__ == '__main__':
+#    counter = 0;
+#    while(counter < 10) :
+    print 'starting tracker'
+    testBlob = BlobTracker()
+    
+    cords = testBlob.searchFrameForColorBlob(255, 100, 52, .15)
+    
+    print cords
+#        cv.NamedWindow("image Picture", 1)
+#        # Display the window
+#        cv.ShowImage("image Picture", cords[1])
+#        cv.waitKey(0);
+#        counter = counter+ 1
