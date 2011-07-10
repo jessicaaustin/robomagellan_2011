93a1f5972366f34082465bb0479716db2ca1c501
bill.mania <bill.mania@191b9dcc-e101-4cc4-a847-655cb88f2929>
2011-07-10 14:53:41 +0000

    new tool for permissions

---
+++ ros/robomagellan/perms.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /opt/projects/robomagellan/ros/robomagellan/nodes
+
+chmod a+x sensorPack.py \
+		  gpsLocation.py \
+		  acceleration.py \
+		  motors.py \
+		  waypoints.py \
+		  navigation.py
+
+cd /opt/projects/robomagellan/ros/robomagellan/test
+
+chmod a+x collision-mock.py
+
+exit 0
