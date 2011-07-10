0409d3a07e2060c2667cda09de66ede4d77c4d24
bill.mania <bill.mania@191b9dcc-e101-4cc4-a847-655cb88f2929>
2011-07-10 14:42:46 +0000

    added checkForCollision

--- ros/robomagellan/nodes/motors.py
+++ ros/robomagellan/nodes/motors.py
@@ -2,9 +2,18 @@
 import roslib; roslib.load_manifest('robomagellan')
 import rospy
 import MotionMindPair
-from robomagellan.msg import Move
+from robomagellan.msg import Move, Collision
 
 motors = MotionMindPair.Mover()
+
+def checkForCollision():
+	collisionState.forwardCollision = motors.checkForwardCollision()
+	collisionState.backwardCollision = motors.checkBackwardCollision()
+
+	if (collisionState.forwardCollision or collisionState.backwardCollision):
+		publisher.publish(collisionState)
+
+	return
 
 def callback(moveMessage):
     rospy.logdebug(rospy.get_name() + ' Left: %d, Right: %d, RampUp: %d',
@@ -12,15 +21,21 @@ def callback(moveMessage):
             moveMessage.rightWheel,
             moveMessage.rampUp)
 
-    motors.move((moveMessage.leftWheel,
-                             moveMessage.rightWheel,
-                             moveMessage.rampUp))
+	checkForCollision()
+	motors.move((moveMessage.leftWheel,
+				 moveMessage.rightWheel,
+				 moveMessage.rampUp))
+	checkForCollision()
+
 
 def controlMotors():
     rospy.init_node('motors', anonymous=True)
     rospy.loginfo(rospy.get_name() + ' Started')
     rospy.Subscriber("motionTopic", Move, callback)
+
     rospy.spin()
 
 if __name__ == '__main__':
-    controlMotors()
+	publisher = rospy.Publisher('collisionTopic', Collision)
+	collisionState = Collision()
+	controlMotors()
